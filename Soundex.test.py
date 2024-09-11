172ccbe716b6826e90a8e9b7b2cf7b6e7bd6ed37
Vijayalaxmi09m <46927317+Vijayalaxmi09m@users.noreply.github.com>
2024-09-11 20:13:21 +0530

    Updated Soundex.test.py with more test cases

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,5 +1,5 @@
 import unittest
-from Soundex import generate_soundex
+from soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
 
@@ -9,6 +9,24 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
 
+    def test_vowels_removed(self):
+        self.assertEqual(generate_soundex("AeIoU"), "A000")
+
+    def test_h_w_y_ignored(self):
+        self.assertEqual(generate_soundex("AhWsy"), "A200")
+
+    def test_duplicate_encodings(self):
+        self.assertEqual(generate_soundex("Abbot"), "A130")
+
+    def test_special_characters(self):
+        self.assertEqual(generate_soundex("Bcd@#E"), "B230")
+
+    def test_names_with_h_w_vowels_between_same_digit_letters(self):
+        self.assertEqual(generate_soundex("Ashcroft"), "A261")
+
+    def test_padding_zeros(self):
+        self.assertEqual(generate_soundex("G"), "G000")
+        self.assertEqual(generate_soundex("Pfister"), "P236")
     
 if __name__ == '__main__':
     unittest.main()
